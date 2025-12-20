75a5a7536c63e7059041cec58d03134ef4f6f3b3
Raz <real.raz.dev@gmail.com>
2025-12-20 21:33:20 +0330

    Update CI configuration

--- src/raztint/__init__.py
+++ src/raztint/__init__.py
@@ -4,20 +4,20 @@ __version__ = "0.1.1"
 
 tint = RazTint()
 
-ok = tint.ok
-err = tint.err
-warn = tint.warn
-info = tint.info
+ok = tint.ok  # type: ignore[attr-defined]
+err = tint.err  # type: ignore[attr-defined]
+warn = tint.warn  # type: ignore[attr-defined]
+info = tint.info  # type: ignore[attr-defined]
 
-black = tint.black
-red = tint.red
-green = tint.green
-yellow = tint.yellow
-blue = tint.blue
-magenta = tint.magenta
-cyan = tint.cyan
-white = tint.white
-gray = tint.gray
+black = tint.black  # type: ignore[attr-defined]
+red = tint.red  # type: ignore[attr-defined]
+green = tint.green  # type: ignore[attr-defined]
+yellow = tint.yellow  # type: ignore[attr-defined]
+blue = tint.blue  # type: ignore[attr-defined]
+magenta = tint.magenta  # type: ignore[attr-defined]
+cyan = tint.cyan  # type: ignore[attr-defined]
+white = tint.white  # type: ignore[attr-defined]
+gray = tint.gray  # type: ignore[attr-defined]
 
 __all__ = [
     "RazTint",
